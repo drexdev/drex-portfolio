1a47b90c0afa0698fc880a287bbcb907d4b72fc3
drexdev <andrelps.amorim17@gmail.com>
2024-07-28 19:36:02 -0300

    feat: Modificando estilização.
    - Adicionado fonte 'Mono Sans';
    - Definido cor de background da aplicação.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'mono': ['"Mono Sans"', 'sans-serif'],
+      },
+      colors: {
+        'background': '#030712',
+      }
+    },
   },
   plugins: [],
 }
